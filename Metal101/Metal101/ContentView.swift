9a7d7054de539640ef321f6db723fe77b98b3f43
Kin Yuunan <yuunan.kin@gmail.com>
2025-02-09 08:02:16 +0900

    pass parameter to shader

--- Metal101/Metal101/ContentView.swift
+++ Metal101/Metal101/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amplitude: Float = 0
     var body: some View {
         VStack {
+            Spacer()
             Text("Color Effect!")
                 .font(.largeTitle)
                 .colorEffect(ShaderLibrary.name1())
             Text("Distortion Effect!")
                 .font(.largeTitle)
-                .distortionEffect(ShaderLibrary.name2(), maxSampleOffset: .zero)
+                .distortionEffect(
+                    ShaderLibrary.SineShader(.float(amplitude)),
+                    maxSampleOffset: .zero
+                )
             Text("Layer Effect!")
                 .font(.largeTitle)
                 .layerEffect(ShaderLibrary.name3(), maxSampleOffset: .zero)
+            Spacer()
+            Slider(value: $amplitude, in: -5...5) {
+                Text("Amplitude: \(amplitude, specifier: "%.1f")")
+            }
         }
         .padding()
     }
